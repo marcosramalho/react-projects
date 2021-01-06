6d9d91de29f31dd8f91424d29f5c2086f119a6d9
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2021-01-06 17:37:00 -0300

    add project 1

---
+++ 1-intro/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  // return React.createElement('a', {href: 'www.google.com' }, 'Google')
+  return <a href="www.google.com">Google</a>
+}
+
+export default App;
