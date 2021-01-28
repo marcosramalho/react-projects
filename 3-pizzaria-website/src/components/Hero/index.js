1b53681730980fce2b715aea9e34ca83e21fb196
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2021-01-28 02:26:55 -0300

    add navbarComponent && add globalStyle && heroComponent

---
+++ 3-pizzaria-website/src/components/Hero/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navbar from '../navbar';
+
+import {
+  HeroContainer,
+  HeroContent,
+  HeroItems,
+  HeroH1,
+  HeroP,
+  HeroBtn,
+} from './heroElements';
+
+const Hero = () => {
+  return (
+    <HeroContainer>
+      <Navbar />
+      <HeroContent>
+        <HeroItems>
+          <HeroH1>Greatest Pizza Ever</HeroH1>
+          <HeroP>Ready in 60 seconds</HeroP>
+          <HeroBtn>Place Order</HeroBtn>
+        </HeroItems>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default Hero;
