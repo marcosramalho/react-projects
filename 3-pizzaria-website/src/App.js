7607af263a5c65edba800f683ad5d11741b12037
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2021-01-28 17:30:07 -0300

    add product

--- 3-pizzaria-website/src/App.js
+++ 3-pizzaria-website/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Hero from './components/Hero';
+import Products from './components/products';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
+import { productData } from './components/products/data';
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
+      <Products heading="Chosse your favorite" data={productData} />
     </Router>
   );
 }
---
+++ 3-pizzaria-website/src/components/products/data.js
@@ -0,0 +1,33 @@
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc:
+      'marinara sauce, basil, italian sausage, roma, tomatoes, olives and pesto',
+    price: 'R$ 29,90',
+    button: 'Add to cart',
+  },
+  {
+    img: product2,
+    alt: 'Pizza',
+    name: 'Hawalian Paradise',
+    desc:
+      'marinara sauce, basil, italian sausage, roma, tomatoes, olives and pesto',
+    price: 'R$ 16,90',
+    button: 'Add to cart',
+  },
+  {
+    img: product3,
+    alt: 'Pizza',
+    name: 'Veggie Overload',
+    desc:
+      'marinara sauce, basil, italian sausage, roma, tomatoes, olives and pesto',
+    price: 'R$ 14,90',
+    button: 'Add to cart',
+  },
+];
