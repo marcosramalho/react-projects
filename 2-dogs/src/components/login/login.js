389a1144d58012627a08b4f1ee2cdc2a529f5b24
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2021-01-31 03:17:32 -0300

    add styles loginForm and login

--- 2-dogs/src/components/login/login.js
+++ 2-dogs/src/components/login/login.js
@@ -8,28 +8,32 @@ import LoginCreate from './loginCreate';
 import LoginPasswordLost from './loginPasswordLost';
 import LoginpasswordReset from './loginPasswordReset';
 
+import styles from './login.module.css';
+
 const Login = () => {
   const { login } = React.useContext(UserContext);
 
   if (login === true) return <Navigate to="/conta" />;
 
   return (
-    <div>
-      <Routes>
-        <Route path="/">
-          <LoginForm />
-        </Route>
-        <Route path="criar">
-          <LoginCreate />
-        </Route>
-        <Route path="perdeu">
-          <LoginPasswordLost />
-        </Route>
-        <Route path="resetar">
-          <LoginpasswordReset />
-        </Route>
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/">
+            <LoginForm />
+          </Route>
+          <Route path="criar">
+            <LoginCreate />
+          </Route>
+          <Route path="perdeu">
+            <LoginPasswordLost />
+          </Route>
+          <Route path="resetar">
+            <LoginpasswordReset />
+          </Route>
+        </Routes>
+      </div>
+    </section>
   );
 };
 
