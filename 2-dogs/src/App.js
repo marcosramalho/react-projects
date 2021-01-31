3c944a261fe42da00db3f4a5cf48eae568cd4c76
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2021-01-31 04:01:01 -0300

    add protected route

--- 2-dogs/src/App.js
+++ 2-dogs/src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { UserStorage } from './userContext';
+
+import './App.css';
+
 import Footer from './components/footer';
 import Header from './components/header';
 import Home from './components/home';
 import Login from './components/login/login';
-
-import { UserStorage } from './userContext';
+import User from './components/user/user';
+import ProtectedRoute from './components/helper/protectedRoute';
 
 function App() {
   return (
@@ -16,6 +19,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
+            <ProtectedRoute path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
