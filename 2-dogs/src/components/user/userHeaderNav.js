5e5b2b2ee5f40305ac1172fc8ab1fda5a2b1f994
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2021-02-21 18:34:54 -0300

    add menu mobile

--- 2-dogs/src/components/user/userHeaderNav.js
+++ 2-dogs/src/components/user/userHeaderNav.js
@@ -1,34 +1,58 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import { UserContext } from '../../userContext';
 import { ReactComponent as MinhasFotos } from '../../assets/feed.svg';
 import { ReactComponent as Estatisticas } from '../../assets/estatisticas.svg';
 import { ReactComponent as AdicionarFoto } from '../../assets/adicionar.svg';
 import { ReactComponent as Sair } from '../../assets/sair.svg';
 import styles from './UserHeaderNav.module.css';
+import useMedia from '../../hooks/userMedia';
 
 const UserHeaderNav = () => {
-  const [mobile, setMobile] = React.useState(null);
   const { userLogout } = React.useContext(UserContext);
+  const mobile = useMedia('(max-width: 40rem)');
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
+
   return (
-    <nav className={styles.nav}>
-      <NavLink to="/conta" end activeClassName={styles.active}>
-        <MinhasFotos />
-        {mobile && 'Minhas Fotos'}
-      </NavLink>
-      <NavLink to="/conta/estatisticas" activeClassName={styles.active}>
-        <Estatisticas />
-        {mobile && 'Estatísticas'}
-      </NavLink>
-      <NavLink to="/conta/postar" activeClassName={styles.active}>
-        <AdicionarFoto />
-        {mobile && 'Adicionar Foto'}
-      </NavLink>
-      <button onClick={userLogout}>
-        <Sair />
-        {mobile && 'Sair'}
-      </button>
-    </nav>
+    <>
+      {mobile && (
+        <button
+          aria-label="Menu"
+          className={`${styles.mobileButton} ${
+            mobileMenu && styles.mobileButtonActive
+          }`}
+          onClick={() => setMobileMenu(!mobileMenu)}
+        ></button>
+      )}
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav} ${
+          mobileMenu && styles.navMobileActive
+        }`}
+      >
+        <NavLink to="/conta" end activeClassName={styles.active}>
+          <MinhasFotos />
+          {mobile && 'Minhas Fotos'}
+        </NavLink>
+        <NavLink to="/conta/estatisticas" activeClassName={styles.active}>
+          <Estatisticas />
+          {mobile && 'Estatísticas'}
+        </NavLink>
+        <NavLink to="/conta/postar" activeClassName={styles.active}>
+          <AdicionarFoto />
+          {mobile && 'Adicionar Foto'}
+        </NavLink>
+        <button onClick={userLogout}>
+          <Sair />
+          {mobile && 'Sair'}
+        </button>
+      </nav>
+    </>
   );
 };
 
--- 3-pizzaria-website/src/components/Hero/index.js
+++ 3-pizzaria-website/src/components/Hero/index.js
@@ -24,9 +24,9 @@ const Hero = () => {
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <HeroContent>
         <HeroItems>
-          <HeroH1>Greatest Pizza Ever</HeroH1>
-          <HeroP>Ready in 60 seconds</HeroP>
-          <HeroBtn>Place Order</HeroBtn>
+          <HeroH1>Pizzaria do GUI</HeroH1>
+          <HeroP>Super saboroso</HeroP>
+          <HeroBtn>Queroooo -)</HeroBtn>
         </HeroItems>
       </HeroContent>
     </HeroContainer>
