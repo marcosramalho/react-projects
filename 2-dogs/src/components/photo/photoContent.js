24e63c830024d77c001a53a51f815e82ebffb0a1
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2021-02-25 17:42:09 -0300

    add delete photo

--- 2-dogs/src/components/photo/photoContent.js
+++ 2-dogs/src/components/photo/photoContent.js
@@ -3,8 +3,11 @@ import styles from './photoContent.module.css';
 
 import { Link } from 'react-router-dom';
 import PhotoComments from './photoComments';
+import { UserContext } from '../../userContext';
+import PhotoDelete from './photoDelete';
 
 const PhotoContent = ({ data }) => {
+  const user = React.useContext(UserContext);
   const { photo, comments } = data;
   return (
     <div className={styles.photo}>
@@ -14,7 +17,12 @@ const PhotoContent = ({ data }) => {
       <div className={styles.details}>
         <div>
           <p className={styles.author}>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
+
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
           <h1 className="title">
