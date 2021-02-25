3959f262219f68db5976b2fb04250a007b015f0f
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2021-02-25 17:52:54 -0300

    add image skeleton

--- 2-dogs/src/components/photo/photoContent.js
+++ 2-dogs/src/components/photo/photoContent.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import PhotoComments from './photoComments';
 import { UserContext } from '../../userContext';
 import PhotoDelete from './photoDelete';
+import Image from '../helper/image';
 
 const PhotoContent = ({ data }) => {
   const user = React.useContext(UserContext);
@@ -12,7 +13,7 @@ const PhotoContent = ({ data }) => {
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
